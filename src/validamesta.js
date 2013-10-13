e8e2f6c2520f865348d153c885f6000790db5757
urzuae <urzuae@gmail.com>
2013-10-12 21:47:36 -0500

    Change plugin structure to a more personal understandable format

--- src/validamesta.js
+++ src/validamesta.js
@@ -1,41 +1,45 @@
 ;(function( $, window, document, undefined) {
-  
-  var pluginName = "validamesta", 
-      defaults    = {
-      };
-  
-  function Plugin( element, options) {
-    this.element = element;
-    this.options = $.extend( {}, defaults, options );
-    this._defaults = defaults;
-    this._name = pluginName;
-    this.init();
-  }
 
-  Plugin.prototype = {
-    init: function() {
-    },
-    validate: function(el, options) {
-      $this = this;
-      $.each(this.element.elements, function(key, val) {
-        if(this.type == 'text') {
-          $this._validate_text(this);
-        }
-      });
-    },
-    _validate_text: function(el) {
-      if("" == el.value) {
-         console.log($(el).css('border'));
-         $(el).css('border', '1px solid red');
-      }
-    }
+  var valid = true;
+  var defaults = {
   };
 
-  $.fn[pluginName] = function( options ) {
-    return this.each(function () {
-      if(!$.data(this, pluginName)) {
-        $.data(this, pluginName, new Plugin(this, options));
+  function validamesta(target, options) {
+    this.$target = $(target);
+    that = this;
+    this.options = $.extend({}, defaults, options);
+    this.init();
+    return valid;
+  }
+  
+  validamesta.prototype = {
+    init: function(options) {
+      if(this.$target.is('form'))
+        this.validateForm();
+      else
+        this.validateField();
+    },
+    validateForm: function() {
+      this.$target.find('input').each(function() {
+        that.validateInput(this);
+      });
+    },
+    validateInput: function(target) {
+      $(target).attr('data-border', $(target).css('border'));
+      if(target.value == "") {
+        valid = false;
+        $(target).css('border', '1px solid red');
       }
+      else
+      {
+        $(target).css('border', $(target).attr('border', $(target).attr('data-border')));
+      }
+    }
+  }
+
+  $.fn.validamesta = function( options ) {
+    return this.each(function() {
+      new validamesta(this, options);
     });
   }
 
