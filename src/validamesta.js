82fb0d9424260da65098436298d97bc42fd336eb
urzuae <urzuae@gmail.com>
2013-10-12 22:42:33 -0500

    Set up first feature for validiting required text elements

--- src/validamesta.js
+++ src/validamesta.js
@@ -2,19 +2,19 @@
 
   var valid = true;
   var defaults = {
+    border: '2px solid #f00'
   };
 
   function validamesta(target, options) {
-    this.$target = $(target);
     that = this;
-    this.options = $.extend({}, defaults, options);
+    this.$target = $(target);
+    this.settings = $.extend({}, defaults, options);
     this.init();
     return valid;
   }
   
   validamesta.prototype = {
     init: function(options) {
-      console.log('validating');
       if(this.$target.is('form'))
         this.validateForm();
       else
@@ -26,16 +26,24 @@
       });
     },
     validateInput: function(target) {
+      if($(target).attr('data-rule') == 'required'){
+        switch(target.type) {
+          case 'text':
+            that._validateText(target);
+        }
+      }
+    },
+    _validateText: function(textTarget) {
+      if(textTarget.value == "")
+        that._invalidField(textTarget);
+      else
+        $(textTarget).css('border', $(textTarget).attr('data-border'));
+    },
+    _invalidField: function(target) {
       //What I do here is to save the border style of the input for the first time and upon fail place a red border
       if($(target).attr('data-border') == "") $(target).attr('data-border', $(target).css('border'));
-      if(target.value == "") {
-        valid = false;
-        $(target).css('border', '1px solid red');
-      }
-      else
-      {
-        $(target).css('border', $(target).attr('border', $(target).attr('data-border')));
-      }
+      valid = false;
+      $(target).css('border', that.settings.border);
     }
   }
 
